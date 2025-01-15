677c39c21d8105ff78a7a954469811e4e194f69b
ThienBao <thienbao3018@gmail.com>
2025-01-15 17:50:08 +0700

    Create RegionsController

---
+++ SQL_IN_PROJECT.sql
@@ -0,0 +1,7 @@
+﻿USE AppApi
+GO
+
+INSERT INTO Regions (Id,Code,Name,RegionImageUrl) VALUES (NEWID(),'R001','North Region','https://example.com/images/north-region.jpg')
+
+SELECT *
+FROM Regions;
---
+++ Web_API/Controllers/RegionsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Web_API.Data;
+
+namespace Web_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly AppApiDbContext _appApiContext;
+        
+        public RegionsController(AppApiDbContext appApiContext)
+        {
+            this._appApiContext = appApiContext;
+        }
+
+        [HttpGet]
+        public IActionResult getAll() { 
+
+            var regions = _appApiContext.Regions.ToList();
+            return Ok(regions);
+        }
+    }
+}
